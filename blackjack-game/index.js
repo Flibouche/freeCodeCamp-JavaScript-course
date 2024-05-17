81debb7e4bdc856736fc1173e4cca42ebd3c51fd
Flibouche <kevin.pfiffer2@gmail.com>
2024-05-17 11:33:03 +0200

    Create blackjack game.

---
+++ blackjack-game/index.js
@@ -0,0 +1,16 @@
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+
+if (sum <= 20) {
+  console.log("Do you want to draw a new card ?");
+} else if (sum === 21) {
+  console.log("Wohoo ! You've got Blackjack !");
+  hasBlackJack = true;
+} else {
+  console.log("You're out of the game !");
+}
+
+// Cash out !
+console.log(hasBlackJack)
