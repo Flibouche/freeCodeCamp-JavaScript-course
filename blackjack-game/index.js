7b00e753bc4db87929ebde7a2b62ebaeeb726578
Flibouche <kevin.pfiffer2@gmail.com>
2024-05-17 12:57:34 +0200

    Create a rollDice function.

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -1,16 +1,54 @@
-let firstCard = 10;
-let secondCard = 11;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let cardsEl = document.getElementById("cards-el");
+let sumEl = document.getElementById("sum-el");
 
-if (sum <= 20) {
-  console.log("Do you want to draw a new card ?");
-} else if (sum === 21) {
-  console.log("Wohoo ! You've got Blackjack !");
-  hasBlackJack = true;
-} else {
-  console.log("You're out of the game !");
+function getRandomCard() {
+  return 5;
 }
 
-// Cash out !
-console.log(hasBlackJack)
+function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards : ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  sumEl.textContent = "Sum : " + sum;
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card ?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack !";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game !";
+    isAlive = false;
+  }
+
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  let card = getRandomCard();
+  sum += card;
+  cards.push(card);
+  console.log(cards);
+  renderGame();
+}
+
+function rollDice() {
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
+  return randomNumber;
+}
+
+console.log(rollDice());
